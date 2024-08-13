92662843cede760067819c9d8275d8496f0fe9d7
picosnenes <alexaregut@gmail.com>
2024-08-13 09:04:29 -0600

    Create article.js

---
+++ article.js
@@ -0,0 +1,20 @@
+import { useRouter } from 'next/router';
+
+export default function Article() {
+    const router = useRouter();
+    const { title } = router.query;
+
+    const articles = JSON.parse(localStorage.getItem('articles')) || [];
+    const article = articles.find((article) => article.title === title);
+
+    if (!article) {
+        return <div>Article not found</div>;
+    }
+
+    return (
+        <div>
+            <h1>{article.title}</h1>
+            <p>{article.content}</p>
+        </div>
+    );
+}
